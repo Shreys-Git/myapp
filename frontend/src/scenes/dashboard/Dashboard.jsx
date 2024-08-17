576333e2ad01099e9ed910df86ddbb2a67832ba8
Shreyash Singh <write.shrey@gmail.com>
2024-08-17 14:26:35 +0100

    sidebar

--- frontend/src/scenes/dashboard/Dashboard.jsx
+++ frontend/src/scenes/dashboard/Dashboard.jsx
@@ -1,3 +1,10 @@
+import { Box } from "@mui/material";
+import { Header } from "../../components/Header";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      <Header title={"DASHBOARD"} subtitle={"Welcome to the Dashboard"} />
+    </Box>
+  );
 };
