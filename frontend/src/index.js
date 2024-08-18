fa4ea25bff0a94a5e80c08ad6b16a860029843ca
Shreyash Singh <write.shrey@gmail.com>
2024-08-18 20:47:32 +0100

    Basic Convex Setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
+{
+  /*TODO: Put this in a env file*/
+}
+const convex = new ConvexReactClient(
+  "https://precious-falcon-631.convex.cloud"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ConvexProvider client={convex}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConvexProvider>
   </React.StrictMode>
 );
--- frontend/src/scenes/dashboard/Dashboard.jsx
+++ frontend/src/scenes/dashboard/Dashboard.jsx
@@ -1,10 +1,30 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Header } from "../../components/Header";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export const Dashboard = () => {
+  const data = useQuery(api.getItem.getTaskList);
+  const createTask = useMutation(api.createItem.createTask);
+
+  const persistData = async () => {
+    try {
+      const id = await createTask({ text: "new testy input1" });
+      console.log("Yay data persisted with id: " + id);
+    } catch (error) {
+      console.error("Error persisting data: ", error);
+    }
+  };
+
   return (
     <Box m="20px">
-      <Header title={"DASHBOARD"} subtitle={"Welcome to the Dashboard"} />
+      <Header
+        title={"DASHBOARD"}
+        subtitle={data ? JSON.stringify(data) : "Loading..."}
+      />
+      <Button variant="contained" color="secondary" onClick={persistData}>
+        Click Me!
+      </Button>
     </Box>
   );
 };
